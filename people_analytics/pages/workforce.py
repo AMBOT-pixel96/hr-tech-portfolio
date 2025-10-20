1221120f4248c76e6e31e333f5eb89771945be42
Amlan Mishra <70195568+AMBOT-pixel96@users.noreply.github.com>
2025-10-20 17:33:41 +0530

    Update workforce.py

--- people_analytics/pages/workforce.py
+++ people_analytics/pages/workforce.py
@@ -3,5 +3,5 @@ import streamlit as st
 from modules.workforce_module import run_workforce_module
 from utils.template_helper import render_download_template
 
-st.set_page_config(page_title="Workforce & Talent Analytics", layout="wide")
+st.set_page_config(page_title="Workforce & Talent", layout="wide")
 run_workforce_module()
