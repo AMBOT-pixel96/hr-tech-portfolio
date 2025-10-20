8924216c8fc6625b356c2915fc8ec6f54d597935
Amlan Mishra <70195568+AMBOT-pixel96@users.noreply.github.com>
2025-10-20 16:29:15 +0530

    Update attrition.py

--- people_analytics/pages/attrition.py
+++ people_analytics/pages/attrition.py
@@ -1,7 +1,6 @@
 # pages/attrition.py
 import streamlit as st
 from modules.attrition_module import run_attrition_module
-from utils.template_helper import render_download_template
 
 st.set_page_config(page_title="Attrition Analytics", layout="wide")
 run_attrition_module()
