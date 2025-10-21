b571f5b4fcad59148a5ec0ca3df12270f404ce97
Amlan Mishra <70195568+AMBOT-pixel96@users.noreply.github.com>
2025-10-21 11:18:07 +0530

    Update attrition.py

--- people_analytics/pages/attrition.py
+++ people_analytics/pages/attrition.py
@@ -4,7 +4,8 @@
 
 import streamlit as st
 from modules.attrition_module import run_attrition_module
-
+from utils.ui_styling import apply_sidebar_theme
+apply_sidebar_theme()
 st.set_page_config(page_title="Attrition Analytics", layout="wide")
 
 # --- Unified Executive Sidebar Styling ---
